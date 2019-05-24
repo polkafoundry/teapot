5556e70cabe0dac43b06300cba757c361cde6a8a
NTPhuong <thaiphuongn1@gmail.com>
2019-05-24 16:56:21 +0700

    bip39

--- src/components/CreateWalletScreen.js
+++ src/components/CreateWalletScreen.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Container, Content, Segment, Text, Icon, Button, Header, Left, Body, Title, Right, Form, Textarea, Input, Item } from 'native-base';
 
+import bip39 from 'react-native-bip39';
+
 export default class CreateWalletScreen extends Component {
     static navigationOptions = {
         title: 'Create Wallet'
+    }
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mnemonic: null
+        }
+    }
+
+    componentWillMount = () => {
+        bip39.generateMnemonic().then(mnemonic => {
+            this.setState({ mnemonic })
+        });
     }
 
     render() {
@@ -14,7 +30,7 @@ export default class CreateWalletScreen extends Component {
                     <View style={{ flex: 1 }}>
                         <Text note>Please copy and backup the 12 mnemonics below. This is very important data to recover your wallet.</Text>
                         <Form>
-                            <Textarea rowSpan={5} bordered disabled />
+                            <Textarea rowSpan={5} bordered disabled value={ this.state.mnemonic }/>
                         </Form>
                     </View>
                     <View style={{ flex: 1 }}>
